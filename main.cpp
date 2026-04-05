1d2addf1f8fdadf9033e632194009f34305098b4
Hervey67 <herv2007@gmail.com>
2026-04-04 20:26:24 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main()
 {
-  int prev, cur;
-  cin >> prev;
-  cur = prev;
+  int prev, cur, flag; 
+  
+  flag == 1;
+  cin >> prev >> cur;
+  cout << prev << " ";
 
-  while (prev <= cur){
-    cout << prev << " " << cur;
+  while ((flag == 1) || (prev > cur)){
+	flag = 0;
+    cout << cur << " ";
     prev = cur;
     cin >> cur;
   }
-
+cout << endl;
 }
